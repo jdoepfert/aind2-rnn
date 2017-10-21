0b083dcd001deb8fff26dfedd31d6b908328e356
Joerg <joerg.doepfert@flixbus.com>
2017-10-21 14:15:45 +0200

    add answers

--- my_answers.py
+++ my_answers.py
@@ -12,6 +12,12 @@ def window_transform_series(series, window_size):
     # containers for input/output pairs
     X = []
     y = []
+    
+    n_windows = len(series) - window_size
+    for start in range(n_windows):
+        end = start + window_size
+        X.append(series[start:end])
+        y.append(series[end])
 
     # reshape each 
     X = np.asarray(X)
@@ -23,13 +29,24 @@ def window_transform_series(series, window_size):
 
 # TODO: build an RNN to perform regression on our time series input/output data
 def build_part1_RNN(window_size):
-    pass
+    model = Sequential()
+    model.add(LSTM(5, input_shape=(window_size, 1)))
+    model.add(Dense(1))
+   
+ # build model using keras documentation recommended optimizer initialization
+    optimizer = keras.optimizers.RMSprop(lr=0.001, rho=0.9, epsilon=1e-08, decay=0.0)
 
+    # compile the model
+    model.compile(loss='mean_squared_error', optimizer=optimizer)
+    return model
 
 ### TODO: return the text input with only ascii lowercase and the punctuation given below included.
+
 def cleaned_text(text):
     punctuation = ['!', ',', '.', ':', ';', '?']
-
+    to_remove = set(text) - set(punctuation) - set(string.ascii_lowercase) - set('0123456789') - set(' ')
+    for char in to_remove:
+        text = text.replace(char, '')
     return text
 
 ### TODO: fill out the function below that transforms the input text and window-size into a set of input/output pairs for use with our RNN model
@@ -37,10 +54,28 @@ def window_transform_text(text, window_size, step_size):
     # containers for input/output pairs
     inputs = []
     outputs = []
+    max_start = len(text) - window_size
+    for start in range(0, max_start, step_size):
+        end = start + window_size
+        inputs.append(text[start:end])
+        outputs.append(text[end])
 
-    return inputs,outputs
+    return inputs, outputs
 
 # TODO build the required RNN model: 
 # a single LSTM hidden layer with softmax activation, categorical_crossentropy loss 
 def build_part2_RNN(window_size, num_chars):
-    pass
+    def build_part2_RNN(window_size, num_chars):
+    model = Sequential()
+    model.add(LSTM(200, input_shape=(window_size, num_chars)))
+    model.add(Dense(num_chars))
+    model.add(Activation('softmax'))
+    
+    # initialize optimizer
+    optimizer = keras.optimizers.RMSprop(lr=0.001, rho=0.9, epsilon=1e-08, decay=0.0)
+
+    # compile model --> make sure initialized optimizer and callbacks - as defined above - are used
+    model.compile(loss='categorical_crossentropy', optimizer=optimizer)
+
+    return model
+
